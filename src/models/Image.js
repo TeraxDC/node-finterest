45d2151a69984044a45388881dd8c6987f7387be
De La Cruz Soldevilla <77691@BCRP.GOB>
2020-02-07 10:22:24 -0500

    funka

--- src/models/Image.js
+++ src/models/Image.js
@@ -8,7 +8,7 @@ const imageSchema = new Schema({
     originalname: {type: String},
     mimetype: {type: String},
     size: { type: Number},
-    created_at: {type: Date, default: Date.now()}
+    created_at: {type: Date, default: new Date()}
 });
 
 module.exports = model('Image', imageSchema);
